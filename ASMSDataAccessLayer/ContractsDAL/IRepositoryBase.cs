117a18e93f0f880da12a1eb1f72038ef75e35496
Uğur Okan Çivgin <37384522+ugurcivgin34@users.noreply.github.com>
2022-04-28 12:10:21 +0300

    Repository yapısı yazıldı

---
+++ ASMSDataAccessLayer/ContractsDAL/IRepositoryBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASMSDataAccessLayer.ContractsDAL
+{
+    public interface IRepositoryBase<T, Id> where T : class, new() //abstract olmayacak yani
+    {
+        IQueryable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeEntities = null);
+        //includeEntities inner join ile ilişkide olduğu entity getirecektir.
+        T GetFirstOrDefault(Expression<Func<T, bool>> filter = null, string includeEntities = null);
+        T GetById(Id id);
+        bool Add(T entity);
+        bool Update(T entity);
+        bool Delete(T entity);
+
+    }
+}
---
+++ ASMSDataAccessLayer/İmplementationsDAL/RepositoryBase.cs
@@ -0,0 +1,139 @@
+﻿using ASMSDataAccessLayer.ContractsDAL;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASMSDataAccessLayer.İmplementationsDAL
+{
+    public class RepositoryBase<T, Id> : IRepositoryBase<T, Id> where T : class, new()
+    {
+        protected readonly MyContext _myContext;
+
+        public RepositoryBase(MyContext myContext)
+        {
+            _myContext = myContext;
+        }
+        public bool Add(T entity)
+        {
+            try
+            {
+
+                _myContext.Set<T>().Add(entity);
+                return _myContext.SaveChanges() > 0 ? true : false;
+                ;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool Delete(T entity)
+        {
+            try
+            {
+                _myContext.Set<T>().Remove(entity);
+                return _myContext.SaveChanges() > 0 ? true : false;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public IQueryable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeEntities = null)
+        {
+            try
+            {
+                IQueryable<T> query = _myContext.Set<T>();
+                if (filter != null)
+                {
+                    query = query.Where(filter);
+                }
+                if (includeEntities != null)
+                {
+                    foreach (var item in includeEntities.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                    {
+                        query = query.Include(item);
+                    }
+                }
+                if (orderBy!=null)
+                {
+                    return orderBy(query);
+                }
+                return query;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public T GetById(Id id)
+        {
+            try
+            {
+                return _myContext.Set<T>().Find(id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter = null, string includeEntities = null)
+        {
+            try
+            {
+                //select * from TableName
+                IQueryable<T> query = _myContext.Set<T>();
+                if (filter != null)
+                {
+                    //select * from TableName where condition
+                    query = query.Where(filter); // where koşulu eklendi.
+                }
+                //ilişkili olduğu tabloyu dahil etmek için (inner join)
+                if (includeEntities != null)
+                {
+
+                    var x = includeEntities.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    foreach (var item in x)
+                    {
+                        //inner join yaptık
+                        query = query.Include(item);
+                    }
+                }
+                return query.FirstOrDefault();
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool Update(T entity)
+        {
+            try
+            {
+                _myContext.Set<T>().Update(entity);
+                return _myContext.SaveChanges() > 0 ? true : false;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
