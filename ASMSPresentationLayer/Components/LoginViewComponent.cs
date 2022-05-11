e9873e33465f6a35773e2b82e2d9f7f87860e5ef
Uğur Okan Çivgin <37384522+ugurcivgin34@users.noreply.github.com>
2022-05-11 13:54:53 +0300

    LoginVİewComponent i oluşturduk , Components klasörünün içine login klasorü oluştururak default html i oluşturduk , ordaki login kısmındaki kodları alıp default un içine yapıştırdık

---
+++ ASMSPresentationLayer/Components/LoginViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using ASMSPresentationLayer.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASMSPresentationLayer.Components
+{
+    public class LoginViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string email)
+        {
+            LoginViewModel model = new LoginViewModel()
+            {
+                Email = email
+            };
+            return View(model);
+        }
+    }
+}
